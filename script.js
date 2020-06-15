9f9daef217968ab2cc468b22b2012160181e4ae5
sirsoco <66388516+sirsoco@users.noreply.github.com>
2020-06-14 19:54:04 -0500

    Password Generator functional

--- script.js
+++ script.js
@@ -1,50 +1,68 @@
-// Assignment Code
+// Assignment of global variables
+
+
 var generateBtn = document.querySelector("#generate");
 var lowercase = "qwertyuiopasdfghjklzxcvbnm";
 var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var numeric = "0123456789";
 var specialCharacters = "!@#^*.";
-var updatePassword = ""
+var passwordLength = "";
+var password = "";
+var updatePassword = "";
 
 
-//Password length request prompt && password generator function 
-function generatePassword() {
-console.log("hello")
-var passwordLength = parseInt(prompt("How many characters between 8-128 would you like your password to be?"))
- 
+//request user for passwordlength and check for invalid statements
+function passwordRequest() {
 
-for ( var i = 0; i < passwordLength; i++) {
-  if( passwordLength > 8 || passwordLength < 128  ) {
+ passwordLength = parseInt(prompt("How many characters between 8-128 would you like your password to be?"))
     console.log(passwordLength)
+    if (passwordLength >=  8 && passwordLength  <= 128 ) { 
+        
+        console.log(passwordLength)
+        selectPassword()
+        }
+         else {
+        alert("Please enter a number between 8 and 128")
+         }
+        }
+
+//request password criteria 
+function selectPassword() {
+
+        console.log(passwordLength)
         if (confirm("Do you want to include a lower case letter?")) {
-          updatePassword += lowercase[Math.floor(Math.random * lowercase.length)]
-          console.log(lowercase)
+          updatePassword += lowercase
             };
         if (confirm("Do you want to include a uppercase case letter?")){
-          updatePassword += uppercase[Math.floor(Math.random * uppercase.length)]
-          console.log(updatePassword);
+          updatePassword += uppercase;
             };
         if (confirm("Do you want to include a number")){
-          updatePassword += numeric[Math.floor(Math.random * numeric.length)]
-          console.log(updatePassword);
+          updatePassword += numeric
              };
         if (confirm("Do you want to include special characters?")) {
-          updatePassword += specialCharacters[Math.floor(Math.random * specialCharacters.length)]
-          console.log(updatePassword);
+        updatePassword += specialCharacters;
             };
-        };
+    console.log(updatePassword);
+    return generatePassword();
     };
-};
-  var passwordText = document.querySelector("#password");
+//build password according to user requested password length 
+function generatePassword() {
 
+    console.log(passwordLength);
 
+        for ( var i = 0; i < passwordLength; i++) {  
+
+           password +=  updatePassword[Math.floor(Math.random() * updatePassword.length)]
+            };
+        console.log(password);
+        return writePassword ();
+        };
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
 
-  passwordText.value = password;
-
+    console.log("writePassword");
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordRequest);
